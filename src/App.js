0ca0e7a17a8f4ef9eafb4f58f678a30fc055a1ea
cragtok <lidfun@protonmail.com>
2022-12-21 17:00:16 +0600

    Move all context and reducer logic to TasksContext.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import { useReducer } from "react";
 import AddTask from "./components/AddTask.js";
 import TaskList from "./components/TaskList.js";
-import tasksReducer from "./tasksReducer.js";
-import { TasksContext, TasksDispatchContext } from "./contexts/TasksContext.js";
+import { TasksProvider } from "./contexts/TasksContext.js";
 
 export default function TaskApp() {
-    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
-
     return (
-        <TasksContext.Provider value={tasks}>
-            <TasksDispatchContext.Provider value={dispatch}>
-                <h1>Prague itinerary</h1>
-                <AddTask />
-                <TaskList />
-            </TasksDispatchContext.Provider>
-        </TasksContext.Provider>
+        <TasksProvider>
+            <h1>Prague itinerary</h1>
+            <AddTask />
+            <TaskList />
+        </TasksProvider>
     );
 }
-
-const initialTasks = [
-    { id: 0, text: "Visit Kafka Museum", done: true },
-    { id: 1, text: "Watch a puppet show", done: false },
-    { id: 2, text: "Lennon Wall pic", done: false },
-];
--- src/contexts/TasksContext.js
+++ src/contexts/TasksContext.js
@@ -1,4 +1,23 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import tasksReducer from "../reducers/tasksReducer";
 
 export const TasksContext = createContext(null);
 export const TasksDispatchContext = createContext(null);
+
+export function TasksProvider({ children }) {
+    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+    return (
+        <TasksContext.Provider value={tasks}>
+            <TasksDispatchContext.Provider value={dispatch}>
+                {children}
+            </TasksDispatchContext.Provider>
+        </TasksContext.Provider>
+    );
+}
+
+const initialTasks = [
+    { id: 0, text: "Visit Kafka Museum", done: true },
+    { id: 1, text: "Watch a puppet show", done: false },
+    { id: 2, text: "Lennon Wall pic", done: false },
+];
